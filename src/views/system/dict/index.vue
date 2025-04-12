cfe493cf1477a88a81573b79f58a711db4002aa2
不做码农 <599854767@qq.com>
2025-04-12 14:21:28 +0800

    :zap:优化字典管理页面

--- src/views/system/dict/index.vue
+++ src/views/system/dict/index.vue
@@ -2,18 +2,18 @@
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch">
       <el-form-item label="字典类型" prop="dictType">
-        <el-input v-model="queryParams.dictType" placeholder="请输入字典类型" clearable @keyup.enter="handleQuery" />
+        <el-input v-model="queryParams.dictType" placeholder="请输入字典类型" @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item label="字典名称" prop="dictName">
-        <el-input v-model="queryParams.dictName" placeholder="请输入字典名称" clearable @keyup.enter="handleQuery" />
+        <el-input v-model="queryParams.dictName" placeholder="请输入字典名称" @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item label="状态" prop="status">
-        <el-select v-model="queryParams.status" placeholder="字典状态" clearable>
+        <el-select v-model="queryParams.status" placeholder="字典状态">
           <el-option v-for="dict in statusOptions" :key="dict.dictValue" :label="dict.dictLabel" :value="dict.dictValue" />
         </el-select>
       </el-form-item>
       <el-form-item label="是否内置" prop="type">
-        <el-select v-model="queryParams.type" placeholder="是否内置" clearable>
+        <el-select v-model="queryParams.type" placeholder="是否内置">
           <el-option v-for="dict in typeOptions" :key="dict.dictValue" :label="dict.dictLabel" :value="dict.dictValue" />
         </el-select>
       </el-form-item>
