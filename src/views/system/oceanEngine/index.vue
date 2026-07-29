9854956be7cf6f93cb81ded21f221b1ee50399f9
t764622507@gmail.com <t764622507@gmail.com>
2026-07-30 01:49:00 +0800

    巨量引擎配置页面

---
+++ src/views/system/oceanEngine/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <el-card shadow="never">
+      <template #header>
+        <span>巨量引擎配置</span>
+      </template>
+
+      <el-empty description="巨量引擎配置页面，用于测试菜单权限和页面配置" />
+    </el-card>
+  </div>
+</template>
+
+<script setup name="OceanEngineConfig">
+</script>
